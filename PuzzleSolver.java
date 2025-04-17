9c5e98343d6d6b54ecfe9c8414ba654f13b5be6c
Mercedes VanArsdale <159818769+mvanarsdale@users.noreply.github.com>
2025-04-17 15:12:44 -0400

    DFS Solver

--- PuzzleSolver.java
+++ PuzzleSolver.java
@@ -1,7 +1,18 @@
 package sudokuSolver;
 
+
+/** 
+* 
+* Class that produces sudoku board solutions
+* April 17, 2025
+*/
+
+
 public class PuzzleSolver {
 	public static void solver(String[] args) {
+		
+		
+
 		
 	}
         
@@ -12,30 +23,142 @@ public class PuzzleSolver {
 		int columnNum = 0;
 		
 		// traversing the board
-		while (columnNum != 8 && rowNum != 8) {
-			if (board.getCell() == 0) {
+		while (rowNum <= 8) {
+			System.out.println(board.getCell());
+			// NUMBER CELL CASES
+			if (board.getCell() != 0) {
 				if (columnNum == 8 && rowNum != 8) {
-					// cell tracker at end of row
+					// MOVER - cell tracker at end of row
 					rowNum++;
 					columnNum = 0;
 					board.moveTo(rowNum, columnNum);
 				} else if (columnNum != 8) {
-					// cell tracker in middle of row 
+					// MOVER - cell tracker in middle of row 
 					columnNum++;
 					board.moveTo(rowNum, columnNum);
 				} else {
-					// solved end of puzzle reached 
-					continue;
+					// solved 
+					System.out.println("finished"); 
+					break;
 				}
+			// EMPTY CELL CASES
+			} else if (board.getCell() == 0){
+				
+				
+				
+				
+				
+				
+				if (columnNum == 8 && rowNum != 8) {
+					int newValue = valueChanger(board);
+					// MOVER- cell tracker at end of row
+					rowNum++;
+					columnNum = 0;
+					board.moveTo(rowNum, columnNum);
+				} else if (columnNum != 8) {
 					
+					// MOVER - cell tracker in middle of row 
+					columnNum++;
+					board.moveTo(rowNum, columnNum);
+				} else {
+					// solved
+					System.out.println("finished");
+					break;
+				}
+				
 			}
-					
-				
 			
-				
+		}
+							
+		}
+	
+	static boolean isValid() {
+		if (validRow() == false) {
+			return false;
 		}
 		
+		if (validBox() == false) {
+			return false;
+		}
+		
+		if (validColumn() == false) {
+			return false;
+		}
+		return true;
+		
+	}
+	
+	// check if row is valid
+	static boolean validRow() {
+		return false;
+		
+	}
+	
+	// check if box is valid 
+	static boolean validBox() {
+		return false;
+		
+	}
+	
+	// check if column is valid
+	static boolean validColumn() {
+		return false;
+		
+	}
+	
+	// find value for cell
+	static int valueChanger(SudokuBoard board) {
+		for (int n = 1; n <= 9; n++) {
+			board.setCell(n);
+			if (isValid() == true) {
+				break;
+			} else {
+				board.setCell(valueChanger(board));
+			}
+			
+		}
+		return 0;	
+		
+	}
+	
+	// will use later on maybe make it like a row and column seperatation 
+	static void mover(SudokuBoard board, int rowNum, int columnNum) {
+		System.out.println(board.getCell());
+		// NUMBER CELL CASES
+		if (board.getCell() != 0) {
+			if (columnNum == 8 && rowNum != 8) {
+				// MOVER - cell tracker at end of row
+				rowNum++;
+				columnNum = 0;
+				board.moveTo(rowNum, columnNum);
+			} else if (columnNum != 8) {
+				// MOVER - cell tracker in middle of row 
+				columnNum++;
+				board.moveTo(rowNum, columnNum);
+			} else {
+				// solved 
+				System.out.println("finished"); 
+			}
+		// EMPTY CELL CASES
+		} else if (board.getCell() == 0){
+			if (columnNum == 8 && rowNum != 8) {
+				
+				// MOVER- cell tracker at end of row
+				rowNum++;
+				columnNum = 0;
+				board.moveTo(rowNum, columnNum);
+			} else if (columnNum != 8) {
+				
+				// MOVER - cell tracker in middle of row 
+				columnNum++;
+				board.moveTo(rowNum, columnNum);
+			} else {
+				// solved
+				System.out.println("finished");
+			}
+			
+		}
 		
 	}
 
-	}
+}

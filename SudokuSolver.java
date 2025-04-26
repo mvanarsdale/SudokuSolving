824f0788b388212a3dd467b2d97ea8553ae7255c
Mercedes VanArsdale <159818769+mvanarsdale@users.noreply.github.com>
2025-04-26 12:49:12 -0400

    Deleting bugged functions
    Will update with working ones when fixed

--- SudokuSolver.java
+++ SudokuSolver.java
@@ -36,68 +36,19 @@ public class SudokuSolver {
     
     
   
-    // Mercedes 
+    	// Mercedes 
 	// code referenced from: YT@Coding with John [https://www.youtube.com/watch?v=mcXc8Mva2bA]
 	// Solve the Sudoku puzzle using DFS and backtracking
 	public boolean DFS_solveBoard(Vertex cell, Graph<Vertex> sudokuGraph) {
-		// solved it or no cells left
-		if (cell == null) {	
-			return true; 
-		}
-		// cell is empty
-		cell = findingEmptyCell(cell, sudokuGraph);
-		if (cell != null) {
-			for (int PosNumber = 1; PosNumber <= 9; PosNumber++) {
-				if (isValid(cell, PosNumber, sudokuGraph) == true) {
-					cell.setValue(PosNumber);
-				} else {
-					DFS_solveBoard(cell, sudokuGraph);
-				}
-				
-			}
-		}		
 	}
 		
 		
 	Vertex findingEmptyCell(Vertex cell, Graph<Vertex> sudokuGraph) {
-		if (cell.getValue() == 0) {
-			return cell; 
-		} else {
 		
-		// column and row variables
-		int col = cell.col;
-		int row = cell.row;
-		
-		// if cell is placed before end of row
-		if (col < 8) {
-			col++;
-		}
-		
-		// cell is at the end of the row
-		if (row == 8) {
-			col = 0;
-			row++;
-		}
-		
-		// move cell to find next zero value
-		cell = findingEmptyCell(Graph.getVertexAt(row, col, sudokuGraph.map), sudokuGraph);
-		
-		}
 }
     
 	boolean isValid(Vertex cell, int number, Graph<Vertex> sudokuGraph) {
-		List<Vertex> neighbors = sudokuGraph.neighbours(cell); // get da neighbors (o´ω｀o)
-	    
-		// check neighbors of the cell
-	    for (Vertex neighbor : neighbors) {
-	    	if (neighbor.getValue() == number) { 
-	            // can't place the number is in neighbors
-	        	return false;
-	        }
-	    }
-	    // no neighbor with that value
-	    return true; 
-	}
+		
 }
 	
 	

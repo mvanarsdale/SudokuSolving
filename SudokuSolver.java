12fa13d351f495cb2dc9dccfeda01e6c192cb262
Mercedes VanArsdale <159818769+mvanarsdale@users.noreply.github.com>
2025-04-26 20:39:40 -0400

    Add files via upload

--- SudokuSolver.java
+++ SudokuSolver.java
@@ -1,9 +1,6 @@
 package sudokuSolver;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 /** 
 * 
@@ -36,73 +33,71 @@ public class SudokuSolver {
     
     
   
-    // Mercedes 
-	// code referenced from: YT@Coding with John [https://www.youtube.com/watch?v=mcXc8Mva2bA]
+	// Mercedes
 	// Solve the Sudoku puzzle using DFS and backtracking
+	// code referenced from: YT@Coding with John [https://www.youtube.com/watch?v=mcXc8Mva2bA]
 	public static boolean DFS_solveBoard(Vertex cell, Graph<Vertex> sudokuGraph) {
+		cell = findingEmptyCell(cell, sudokuGraph);
 		// solved it or no cells left
 		if (cell == null) {	
 			// printing solved board
+			//System.out.println("_______________________");
 			BoardBuilder.printSudokuGraph(sudokuGraph);
 			
 			return true; 
 		}
-		// cell is empty
-		cell = findingEmptyCell(cell, sudokuGraph);
-		if (cell != null) {
-			for (int PosNumber = 1; PosNumber <= 9; PosNumber++) {
-				if (isValid(cell, PosNumber, sudokuGraph) == true) {
-					cell.setValue(PosNumber);
-				} else {
-					DFS_solveBoard(cell, sudokuGraph);
-				}
+		
+		// tries placing numbers 1-9
+		for (int PosNumber = 1; PosNumber <= 9; PosNumber++) {
+			// if number is valid set cells value
+			if (isValid(cell, PosNumber, sudokuGraph) == true) {
+				cell.setValue(PosNumber);
+				//BoardBuilder.printSudokuGraph(sudokuGraph);
 				
+				// check overall solution
+				if (DFS_solveBoard(cell, sudokuGraph)) {
+					return true;
+				}
 			}
+			// Backtrack
+            cell.setValue(0);
 		}
+		
 		// no solution
 		return false;		
 	}
 		
 		
-	// finding next empty cell in the board
+	// finding empty cell in the board
 	static Vertex findingEmptyCell(Vertex cell, Graph<Vertex> sudokuGraph) {
-		if (cell.getValue() == 0) {
-			return cell; 
-		} else {
+		// column and row variables
+		int col = cell.col;
+		int row = cell.row;
 		
-			// column and row variables
-			int col = cell.col;
-			int row = cell.row;
-		
-			// if cell is placed before end of row
-			if (col < 8) {
-				col++;
-			}
-		
-			// cell is at the end of the row
-			if (row == 8) {
+		// end of the row hasn't been reached
+		while (col < 9) {
+			// if value is 0 that means its empty
+			if (Graph.getVertexAt(row, col, sudokuGraph.map).getValue() == 0) {
+	            // return empty cell
+				return Graph.getVertexAt(row, col, sudokuGraph.map);
+	        }
+			// move over horizontally
+			col++;
+			// checks if moved to far
+			if (col == 9) {
+				// resets accordingly
 				col = 0;
 				row++;
 			}
-		
-			// move cell to find next zero value
-			cell = findingEmptyCell(Graph.getVertexAt(row, col, sudokuGraph.map), sudokuGraph);
+			// returns null if gone through whole board
+			if (row == 9) {
+				return null;
+			}
 		}
-		return cell;
-	}
-	
-	// code to find first empty cell from chatGPT
-	public static Vertex findFirstEmptyCell(Graph<Vertex> sudokuGraph) {
-	    for (Vertex v : sudokuGraph.map.keySet()) {
-	        if (v.getValue() == 0) {
-	        	// found empty cell
-	        	return v; 
-	        }
-	    }
-	    // reached end of board 
-	    return null; 
+    return null;
 	}
     
+	// checks if number value is in current cells neighbors 
 	static boolean isValid(Vertex cell, int number, Graph<Vertex> sudokuGraph) {
 		// get list of neighboring nodes
 		List<Vertex> neighbors = sudokuGraph.neighbours(cell); 
